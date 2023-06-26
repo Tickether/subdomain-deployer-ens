3eddaf655b9c8fa1bf0cd526a8074061498e220a
Tickether <tickether@gmail.com>
2023-06-26 22:18:11 +0000

    basic wagmi viem W3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,37 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { polygon, polygonMumbai } from 'wagmi/chains'
+
+
+// Wagmi Config
+const chains = [polygon, polygonMumbai];
+const projectId = process.env.NEXT_PUBLIC_W3M_PROJECT_ID
+
+const { publicClient } = configureChains(
+  chains,
+  [w3mProvider({ projectId })],
+)
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({
+    chains,
+    projectId 
+  }),
+  publicClient,
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <WagmiConfig config={wagmiConfig} >
+      <Component {...pageProps} />
+    </WagmiConfig>
+    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+  </>
+  )
 }
